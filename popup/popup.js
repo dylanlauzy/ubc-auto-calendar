1d2c1012444a4adda6aeb2822798f95f0b06661f
dylanlauzy <dylzylau@gmail.com>
2023-01-06 10:50:04 -0800

    retrieving oauth token enabled

--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,7 @@
-const button = document.querySelector('button')
-const responseElem = document.querySelector('p')
+const button = document.querySelector("button");
+const responseElem = document.querySelector("p");
 
 const launchAuth = () => {
-  button.textContent="hello"
-
   let authUrl = "https://accounts.google.com/o/oauth2/auth?";
 
   const manifest = chrome.runtime.getManifest();
@@ -25,17 +23,45 @@ const launchAuth = () => {
   chrome.identity.launchWebAuthFlow(
     { url: authUrl, interactive: true },
     (responseUrl) => {
-      let token = responseUrl.split("=")[1]
-      responseElem.textContent = token
+      let token = responseUrl.split("=")[1];
+      responseElem.textContent = token;
 
-      // chrome.identity.removeCachedAuthToken({
-      //   token: token,
-      // }, () => {})
-      
-      responseElem.textContent = responseUrl
-      console.log(responseUrl)
+      //details about the event
+      let event = {
+        summary: "Test Event",
+        description: "testing...",
+        start: {
+          dateTime: "2023-01-05T09:00:00-07:00",
+          timeZone: "America/Los_Angeles",
+        },
+        end: {
+          dateTime: "2015-05-28T09:00:00-07:00",
+          timeZone: "America/Los_Angeles",
+        },
+      };
+
+      let fetch_options = {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(event),
+      };
+
+      fetch(
+        "https://www.googleapis.com/calendar/v3/calendars/primary/events",
+        fetch_options
+      )
+        .then((response) => response.json()) // Transform the data into json
+        .then(function (data) {
+          console.log(data); //contains the response of the created event
+        });
+
+      responseElem.textContent = responseUrl;
+      console.log(responseUrl);
     }
   );
 };
 
-button.addEventListener('click', launchAuth)
+button.addEventListener("click", launchAuth);
